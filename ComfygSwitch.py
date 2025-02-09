c5142fde8f477000bc646bcc8b9207bb1e01de22
Gabriel Guerreiro <388010+guerreiro@users.noreply.github.com>
2025-02-09 15:41:21 +0000

    Refiner Steps + CKPT names output + ReadMe update

--- ComfygSwitch.py
+++ ComfygSwitch.py
@@ -3,6 +3,14 @@ import folder_paths
 import json
 import os
 
+class AlwaysComparisonReturn(str):
+    def __eq__(self, other: object) -> bool:
+        return True
+    def __ne__(self, other: object) -> bool:
+        return False
+    def __repr__(self) -> str:
+        return "<AlwaysComparisonReturn>"
+
 class ComfygSwitch:
     @classmethod
     def load_configs(cls):
@@ -26,6 +34,7 @@ class ComfygSwitch:
                 "checkpoint_model": (sorted(folder_paths.get_filename_list("checkpoints")),),
                 "use_custom_input": ("BOOLEAN", {"default": False}),
                 "steps": ("INT", {"default": 30, "min": 1, "max": 200}),
+                "refiner_steps": ("INT", {"default": 30, "min": 1, "max": 200}),
                 "cfg": ("FLOAT", {"default": 7.0, "min": 0.1, "max": 20.0, "step": 0.1}),
                 "sampler": (comfy.samplers.KSampler.SAMPLERS, ),
                 "scheduler": (comfy.samplers.KSampler.SCHEDULERS, ),
@@ -33,16 +42,18 @@ class ComfygSwitch:
         }
 
     RETURN_TYPES = (
+        AlwaysComparisonReturn(),
+        "INT",
         "INT",
         "FLOAT",
         comfy.samplers.KSampler.SAMPLERS,
         comfy.samplers.KSampler.SCHEDULERS
     )
-    RETURN_NAMES = ("STEPS", "CFG", "SAMPLER", "SCHEDULER")
+    RETURN_NAMES = ("MODEL_NAME", "STEPS", "REFINE_STEPS", "CFG", "SAMPLER", "SCHEDULER")
     FUNCTION = "select_config"
     CATEGORY = "Configuration"
 
-    def select_config(self, checkpoint_model, use_custom_input, steps, cfg, sampler, scheduler):
+    def select_config(self, checkpoint_model, use_custom_input, steps, refiner_steps, cfg, sampler, scheduler):
         """
         If use_custom_input is True, output the manually entered values.
         Otherwise, load the configuration corresponding to the selected model_choice.
@@ -50,22 +61,27 @@ class ComfygSwitch:
         configs = self.load_configs()
         model_choice = self.get_last_path_segment(checkpoint_model)
         config = configs.get(model_choice, {})
-        print('ComfygSwitch -> model_choice', model_choice)
+        # print('ComfygSwitch -> checkpoint_model', checkpoint_model)
+        print('ComfygSwitch -> model', model_choice)
 
         # Define a default configuration.
         default_config = {
             "steps": steps,
+            "refiner_steps": refiner_steps,
             "cfg": cfg,
             "sampler": sampler,
             "scheduler": scheduler
         }
         final_config = {**default_config, **config}
+        print('ComfygSwitch -> final_config', final_config)
 
         if use_custom_input:
-            return (steps, cfg, sampler, scheduler)
+            return (checkpoint_model, steps, refiner_steps, cfg, sampler, scheduler)
         else:
             return (
+                checkpoint_model,
                 final_config["steps"],
+                final_config["refiner_steps"],
                 final_config["cfg"],
                 final_config["sampler"],
                 final_config["scheduler"]
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# comfyg-switch
-ComfyUI  model based configuration switch
+# Comfyg Switch
+
+Comfyg Switch is a custom node that dynamically selects model configuration parameters based on the chosen checkpoint. 
+It reads model-specific settings from a JSON file (model_configs.json).
+
+## Inputs
+
+- checkpoint_model: This value is used to determine which configuration to load.
+- use_custom_input (BOOLEAN): Toggle between using manual inputs and automatically loaded configurations.
+- steps (INT): Number of inference steps (default: 30).
+- refiner_steps (INT): Number of inference steps for enhancement (default: 30).
+- cfg (FLOAT): Classifier-free guidance scale (default: 7.0).
+- sampler (SAMPLER)
+- scheduler (SCHEDULER)
+
+## Outputs
+
+- MODEL_NAME
+- STEPS (INT)
+- REFINE_STEPS (INT)
+- CFG (FLOAT)
+- SAMPLER (SAMPLER)
+- SCHEDULER (SCHEDULER)
+
+## Example
+
+See the file: ComfygSwitch-example.json
+
+# Contributing
+
+Contributions and suggestions are welcome! If you encounter any issues or have ideas for improvements, please open an issue or submit a pull request on the ComfyUI GitHub repository.
+
+# License
+
+This project is licensed under the MIT License.
